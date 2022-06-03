fd56c99ffa80cde644d73ef71b131013bb84d65e
florvillalba21 <florvllb33@gmail.com>
2022-06-03 12:34:32 -0300

    actualizacion

--- tareas.js
+++ tareas.js
@@ -1,5 +1,22 @@
-var tareasList=[];
+var tareas = [
+    {
+        nya: "Flor Villalba",
+        tarea : "hacer tarea",
+        estado : "completado"
+    }
+]
 
-function listarTareas(){  
-    
+function validarFormulario(){
+    let nya = document.getElementById("nya").value
+    let tarea = document.getElementById("descTarea").value
+    let estado = document.getElementById("estadoTarea").value
+
+    if(nya== "" || tarea=="" || estado == "null"){
+        alert("No se permiten campos vacios o nulos")
+
+    } else{
+        return true;
+
+    }
 }
+
